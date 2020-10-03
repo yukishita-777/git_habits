35a7f59b6eae73daaf4de9de6da2e778615cf567
EC2 Default User <ec2-user@ip-172-31-44-15.ap-northeast-1.compute.internal>
2020-10-03 06:14:55 +0000

    user habitのアソシエーション定義

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -1,2 +1,4 @@
 class Habit < ApplicationRecord
+    # association
+    belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   # association
+  has_many :habits
 end
