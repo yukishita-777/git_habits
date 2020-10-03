9beb3ab35fdd3d75789b8ba77c07b05e254e7fc9
EC2 Default User <ec2-user@ip-172-31-44-15.ap-northeast-1.compute.internal>
2020-10-03 14:32:44 +0000

    createアクション定義

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -1,4 +1,13 @@
 class HabitsController < ApplicationController
     def new
-    end 
+    end
+    
+    def create
+        Habit.create(habit_params)
+    end
+    
+    private
+    def habit_params
+        params.permit(:title, :purpose, :reason)
+    end
 end
