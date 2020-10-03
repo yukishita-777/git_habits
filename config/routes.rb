34e3cc5118effb52473fa196954c960ebf457270
EC2 Default User <ec2-user@ip-172-31-44-15.ap-northeast-1.compute.internal>
2020-10-03 05:33:45 +0000

    ログイン画面をトップページに追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+# rootをログイン画面に設定
+  devise_scope :user do
+    root "users/sessions#new"
+  end
+
+  devise_for :users, :controllers => {
+    sessions: 'users/sessions'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
