bd934ca56f861d483c9dec7492171e1dc96063c5
EC2 Default User <ec2-user@ip-172-31-44-15.ap-northeast-1.compute.internal>
2020-10-03 06:18:47 +0000

    habit ルーティング定義

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
     sessions: 'users/sessions'
   }
   resources :users, only:[:show]
+  resources :habits, only:[:new, :update
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
