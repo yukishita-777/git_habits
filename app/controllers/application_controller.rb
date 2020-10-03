e53db695c3db80de98c5c5f20685b04f9bc42b31
EC2 Default User <ec2-user@ip-172-31-44-15.ap-northeast-1.compute.internal>
2020-10-03 06:00:08 +0000

    ニックネームを保存可能に変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_prameters, if: :devise_controller?
+    
+    def configure_permitted_prameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    end
+        
+    def after_sign_in_path_for(resource)
+        
+    end
 end
