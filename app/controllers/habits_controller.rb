906153aeb3ca9475a72a1b35d61467ee569bd6a1
EC2 Default User <ec2-user@ip-172-31-44-15.ap-northeast-1.compute.internal>
2020-10-05 13:44:34 +0000

    プロフィール編集追加

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -1,4 +1,8 @@
 class HabitsController < ApplicationController
+    def index
+        @habits = Habit.all
+    end
+    
     def new
         @habit = Habit.new
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,15 @@ class UsersController < ApplicationController
     def edit
         @user = User.find(current_user.id)
     end
+    
+    def update
+        @user = User.find(current_user.id)
+        User.update(user_params) if @user.id == current_user.id
+        redirect_to action: :show
+    end
+    
+    private
+    def user_params
+        params.require(:user).permit(:nickname)
+    end
 end
