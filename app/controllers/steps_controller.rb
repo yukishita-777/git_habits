ae7da5b15c9844bc84c9e963bc8da8f4c2457771
EC2 Default User <ec2-user@ip-172-31-44-15.ap-northeast-1.compute.internal>
2020-10-09 15:03:38 +0000

    step 編集、削除追加

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -19,6 +19,18 @@ class StepsController < ApplicationController
         redirect_to controller: :users, action: :show, id: current_user.id
     end
     
+    def update
+        @step = Step.find(params[:id])
+        @step.update(step_params)
+        redirect_to action: :show
+    end
+    
+    def destroy
+        @step = Step.find(params[:id])
+        @step.destroy
+        redirect_to action: :show
+    end
+    
     private
     def step_params
         params.require(:step).permit(:action, :schedule).merge(habit_id: params[:habit_id], user_id: current_user.id)
